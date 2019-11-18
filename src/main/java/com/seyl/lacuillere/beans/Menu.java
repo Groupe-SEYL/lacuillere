8db0b8c22e66baa222e2dba48568a54f96529cc5
Shadi Boomi <shadi.boomi@gmail.com>
2019-11-19 00:08:05 +0100

    Added arguments, empty constructors and getters/setters for each of the beans

--- src/main/java/com/seyl/lacuillere/beans/Menu.java
+++ src/main/java/com/seyl/lacuillere/beans/Menu.java
@@ -1,4 +1,80 @@
 package com.seyl.lacuillere.beans;
 
 public class Menu {
+    private Long id;
+    private String name;
+    private String description;
+    private Dish entree;
+    private Dish mainCourse;
+    private Dish dessert;
+    private float totalPrice;
+    private Drink drink;
+
+    public Menu() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Dish getEntree() {
+        return entree;
+    }
+
+    public void setEntree(Dish entree) {
+        this.entree = entree;
+    }
+
+    public Dish getMainCourse() {
+        return mainCourse;
+    }
+
+    public void setMainCourse(Dish mainCourse) {
+        this.mainCourse = mainCourse;
+    }
+
+    public Dish getDessert() {
+        return dessert;
+    }
+
+    public void setDessert(Dish dessert) {
+        this.dessert = dessert;
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = entree.getPrice() + mainCourse.getPrice() + dessert.getPrice();
+        this.totalPrice = totalPrice;
+    }
+
+    public Drink getDrink() {
+        return drink;
+    }
+
+    public void setDrink(Drink drink) {
+        this.drink = drink;
+    }
 }
--- src/main/java/com/seyl/lacuillere/beans/Restaurant.java
+++ src/main/java/com/seyl/lacuillere/beans/Restaurant.java
@@ -1,4 +1,63 @@
 package com.seyl.lacuillere.beans;
 
+import java.util.List;
+
 public class Restaurant {
+    private Long id;
+    private String name;
+    private String description;
+    private List<Menu> menu;
+    private String address;
+    private int starsNumber;
+
+    public Restaurant() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Menu> getMenu() {
+        return menu;
+    }
+
+    public void setMenu(List<Menu> menu) {
+        this.menu = menu;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getStarsNumber() {
+        return starsNumber;
+    }
+
+    public void setStarsNumber(int starsNumber) {
+        this.starsNumber = starsNumber;
+    }
 }
