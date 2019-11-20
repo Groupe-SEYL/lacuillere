203d4b67d0754ca18d884a5d5247872875275d66
IN-SO-023 <elnaeleanor@gmail.com>
2019-11-20 10:24:59 +0100

    RestController Restaurant : List

--- src/main/java/com/seyl/lacuillere/repository/RestaurantRepository.java
+++ src/main/java/com/seyl/lacuillere/repository/RestaurantRepository.java
@@ -3,5 +3,9 @@ package com.seyl.lacuillere.repository;
 import com.seyl.lacuillere.beans.Restaurant;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RestaurantRepository  extends CrudRepository<Restaurant, Long> {
+
+    List<Restaurant> saveAll();
 }
