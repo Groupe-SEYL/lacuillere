b58f979417a4c0e5484e92dc2a5481891efe2471
Louis <louisdurandy@yahoo.fr>
2019-11-22 12:25:33 +0100

    Modify CrossOrigin Menu

--- src/main/java/com/seyl/lacuillere/restcontroller/RestaurantRestController.java
+++ src/main/java/com/seyl/lacuillere/restcontroller/RestaurantRestController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/restaurants")
-//@CrossOrigin(origins = "http://localhost:4242")
+@CrossOrigin(origins = "http://localhost:4242")
 public class RestaurantRestController {
 
 
