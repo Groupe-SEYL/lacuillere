cc882d0fcd6e4c05d8396d4bbf794251e78473a9
Shadi Boomi <shadi.boomi@gmail.com>
2019-11-28 16:27:17 +0100

    Added first mock tests on the RestaurantControllerClass
    Added a maven plugin on the POM.xml file so Jenkins doesn't have any JDK errors.

--- src/test/java/com/seyl/lacuillere/lacuillere/restcontroller/RestaurantRestControllerTest.java
+++ src/test/java/com/seyl/lacuillere/lacuillere/restcontroller/RestaurantRestControllerTest.java
@@ -1,4 +1,18 @@
 package com.seyl.lacuillere.lacuillere.restcontroller;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class RestaurantRestControllerTest {
+
+    @Test
+    public final void whenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
+        //Assert.assertEquals(3, StringCalculator.add("3"));
+    }
+
+    @Test
+    public final void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
+        //Assert.assertEquals(3+6, StringCalculator.add("3,6"));
+    }
+
 }
