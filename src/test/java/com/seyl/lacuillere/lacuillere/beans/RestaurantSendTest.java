ae98d3f75ab7ff052c57c51e9d99592b78665b50
geward10000 <louisdurandy@yahoo.fr>
2019-11-29 08:30:32 +0100

    Test empty constructor

---
+++ src/test/java/com/seyl/lacuillere/lacuillere/beans/RestaurantSendTest.java
@@ -0,0 +1,17 @@
+package com.seyl.lacuillere.lacuillere.beans;
+
+import com.seyl.lacuillere.beans.RestaurantSend;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+
+public class RestaurantSendTest {
+
+    private RestaurantSend restaurantSend = new RestaurantSend();
+
+    @Test
+    public void testDefaultConstructor() {
+        assertNull(restaurantSend);
+    }
+
+}
